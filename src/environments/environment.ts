190f5f004ac75b50298a7d25d08d558dae13a330
Eduardo-Caza <ecaanaranjo@gmail.com>
2024-11-23 17:37:49 -0500

    borrando credenciales

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,13 +5,13 @@
 export const environment = {
   production: false,
   firebaseConfig : {
-    apiKey: "AIzaSyCeSVOj8OOURSFP_f0dKkUC1vJvCKRFr3s",
-    authDomain: "prueba-dfe13.firebaseapp.com",
-    projectId: "prueba-dfe13",
-    storageBucket: "prueba-dfe13.firebasestorage.app",
-    messagingSenderId: "568930706063",
-    appId: "1:568930706063:web:be9ce17a4418292536c6fa",
-    measurementId: "G-K1CL20HGWM"
+    apiKey: "",
+    authDomain: "",
+    projectId: "",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   }
 };
 
